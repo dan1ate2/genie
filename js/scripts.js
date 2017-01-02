4eac2a4a1459577888775572cb7701930ccecc72
Dan Hogan <dan1ate2@gmail.com>
2017-01-02 23:35:28 +1000

    Problem with trying to reference a property from a variable value

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,87 @@
-// ititialise the robot object
+// create the empty robot object
 var robot = {
 	position:{x:"", y:""},
 	orientation:""
 };
 
 // sets the robot position and orientation from 'place' button
-function setRobotPosition() {
-	var x = document.getElementById("x-pos").value;
-	var y = document.getElementById("y-pos").value;
-	var orientation = document.getElementById("orientation");
-
-	// validation for x and y
-	if (isNaN(x)) { // if x is not a number
-		alert("x must be numeric and between 0-4")
+function placeRobot() {
+	// var x = document.getElementById("x-pos").value;
+	// var y = document.getElementById("y-pos").value;
+	var newPosition = {
+		x: document.getElementById("x-pos").value,
+		y: document.getElementById("y-pos").value
 	}
-	else if ((robot.position.x == "") && (x == "")) { // if x not initialised
-		alert("x cannot be empty, please enter a number between 0-4");
+	var newOrientation = document.getElementById("orientation");
+	var positionValid = false; // flag
+	var orientationValid = false; // flag
+
+	// place x and y in new position
+	Object.keys(newPosition).forEach(function (key) {
+		var tempKeyName;
+		
+		// check if x or y
+		do {
+			switch(key) {
+				case "x": // validate x position
+					tempKeyName = "X Position";
+					positionValid = validatePositionInput(tempKeyName, key, newPosition.x);
+					break;
+				case "y": // validate y position
+					tempKeyName = "Y Position";
+					positionValid = validatePositionInput(tempKeyName, key, newPosition.y);
+					break;
+				default:
+					tempKeyName = ""; // avoids undefined error
+			}
+		}
+		while (positionValid)
+	});
+
+	// check if orientation given or already set
+	if (robot.orientation == "" && newOrientation == "") {
+		orientationValid = false;
+		alert("An orientation must be chosen for the robot to face, please fix.")
 	}
 	else {
-		robot.position.x = x;
+		orientationValid = true;
 	}
 
-	//test iterate over position keys (property names)
-	Object.keys(robot.position).forEach(function (key) {
-		console.log(key);
-	});
+	if (positionValid && orientationValid) {
+		setRobotLocation(newPosition.x, newPosition.y, newOrientation)
+	}
 
 	// [debug]
 	// console.log(orientation.value);
 	// console.log(y);
 	// console.log(x);
+}
+
+// input validation for x or y position
+function validatePositionInput(keyName, key, posValue) {
+	// check for invalid or empty numbers
+	if (isNaN(posValue)) { // if it's not a number
+		alert(keyName + " must be numeric and between 0-4")
+		return false;
+	}
+										else if (robot.position.key == "" && posValue == "") { // if it's not initialised at all
+		alert(keyName + " cannot be empty, please enter a number between 0-4");
+	 	return false;
+	}
+	else {
+		return true; // must be valid
+	}
+}
+
+// update robot object with new x & y position, and orientation
+function setRobotLocation(newX, newY, newOrient) {
+	if (!newX == "") {
+		robot.position.x = newX;
+	}
+	if (!newY == "") {
+		robot.position.y = newY;
+	}
+	if (!newOrient == "") {
+		robot.orientation = newOrient;
+	}
 }
