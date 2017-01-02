5c033fa0adb0e0a10e1eecc36eae1a8be958f78c
Dan Hogan <dan1ate2@gmail.com>
2017-01-02 21:28:57 +1000

    Validation for input (still working on)

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,33 @@
-function initRobot() {
-	var x = document.createRobot.elements["X-axis"].value;
-	var y = document.createRobot.elements["Y-axis"].value;
+// ititialise the robot object
+var robot = {
+	position:{x:"", y:""},
+	orientation:""
+};
 
+// sets the robot position and orientation from 'place' button
+function setRobotPosition() {
+	var x = document.getElementById("x-pos").value;
+	var y = document.getElementById("y-pos").value;
+	var orientation = document.getElementById("orientation");
 
+	// validation for x and y
+	if (isNaN(x)) { // if x is not a number
+		alert("x must be numeric and between 0-4")
+	}
+	else if ((robot.position.x == "") && (x == "")) { // if x not initialised
+		alert("x cannot be empty, please enter a number between 0-4");
+	}
+	else {
+		robot.position.x = x;
+	}
+
+	//test iterate over position keys (property names)
+	Object.keys(robot.position).forEach(function (key) {
+		console.log(key);
+	});
+
+	// [debug]
+	// console.log(orientation.value);
+	// console.log(y);
+	// console.log(x);
 }
